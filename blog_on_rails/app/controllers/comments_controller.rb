5577d3a6494fd18a841396037d615c493f51a10b
Aljan Agamao <agamaoaljan@gmail.com>
2019-07-09 13:15:25 -0700

    fixed column

--- blog_on_rails/app/controllers/comments_controller.rb
+++ blog_on_rails/app/controllers/comments_controller.rb
@@ -18,7 +18,6 @@ class CommentsController < ApplicationController
         redirect_to post_path(@comment.post)
     end
     
-
     private
     def comment_params
         params.require(:comment).permit(:body)
