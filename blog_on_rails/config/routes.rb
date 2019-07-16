016fd4bbbb2a3d50a59d3e204fe07f6bf1568603
Aljan Agamao <agamaoaljan@gmail.com>
2019-07-16 15:52:44 -0700

    added sessions controller, added session view file sign in

--- blog_on_rails/config/routes.rb
+++ blog_on_rails/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
 
 resources :users, only: [:create, :new, :edit]
+resources :sessions, only: [:create, :new, :destroy]
 resources :posts do
   resources :comments, only: [:create, :destroy]
 end
